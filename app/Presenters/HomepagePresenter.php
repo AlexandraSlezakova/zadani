33a4b32581d1fc3448ba1373d101b483cb086b84
Alexandra <alexandra.slezak@gmai.com>
2020-07-02 14:33:36 +0200

    homepage presenter

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -4,9 +4,53 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Model\ChannelGroupsModel;
+use App\Model\ChannelsModel;
 use Nette;
+use Nette\Application\UI\Form;
 
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+    /**
+     * @var ChannelsModel @inject
+     */
+    public $channelsModel;
+
+    /**
+     * @var ChannelGroupsModel @inject
+     */
+    public $channelGroupModel;
+
+    private $channelGroups = [];
+
+    public function renderDefault()
+    {
+        $groups = $this->channelGroupModel->getItems()->order("order");
+
+        foreach ($groups as $group) {
+            foreach ($group->related('Channels.channelGroup') as $channel) {
+                $this->channelGroups[$group->name][] = $channel;
+            }
+        }
+
+        $this->template->channelGroups = $this->channelGroups;
+    }
+
+    protected function createComponentFilterForm(): Form
+    {
+        $form = new Form;
+
+        $form->addText('channelGroup', 'Skupina');
+        $form->addText('name', 'Názov');
+        $form->addText('description', 'Popis');
+
+        return $form;
+    }
+
+    public function handleInputChange($inputId, $value)
+    {
+        
+    }
+
 }
