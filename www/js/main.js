bae879e991f2d75d1cbfaa7c62339728dea1410d
Alexandra <alexandra.slezak@gmai.com>
2020-07-12 02:14:57 +0200

    strankovanie v tabulke

--- www/js/main.js
+++ www/js/main.js
@@ -1,10 +1,38 @@
-$(document).ready(function() {
+$(function () {
     $.nette.init();
+});
 
+$(document).ready(function() {
     $('#snippet--tableSnippet').DataTable({
         "pagingType": "full_numbers",
         "searching": false,
         "ordering": false,
         "sDom": 'Rfrtlip',
-    } );
+    });
+
+    $('input').on('input', function() {
+        $("#frm-filterForm").submit();
+    });
+
+    $("#frm-filterForm").on("submit", function() {
+        let form = $(this);
+
+        $.nette.ajax({
+            url: form.attr("action"),
+            method: "POST",
+            data: form.serialize(),
+            complete: function () {
+                let table = $('#snippet--tableSnippet');
+                table.DataTable().destroy();
+                table.DataTable({
+                    "pagingType": "full_numbers",
+                    "searching": false,
+                    "ordering": false,
+                    "sDom": 'Rfrtlip',
+                } )
+            }
+        });
+
+        return false;
+    });
 });
